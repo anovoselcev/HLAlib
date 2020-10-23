844e22a295ace31f0449b77a97aee2025a64e405
smilemaxdev <novoseltsev.as@phystech.edu>
2020-10-23 21:56:43 +0300

    HLAlib 23.10.2020. SimFederate updated.

--- HLA-Simulink/SimFederate/SimFederate.cpp
+++ HLA-Simulink/SimFederate/SimFederate.cpp
@@ -1,10 +1,11 @@
 #include "SimFederate.hpp"
+#include "Tools/Logger.hpp"
 
 namespace HLA {
     using namespace  rti1516e;
 
 
-//    SimFederate::SimFederate() noexcept{}
+    SimFederate::SimFederate() noexcept{}
 
     SimFederate::SimFederate(const std::wstring& name,
                              const std::wstring& type,
@@ -21,9 +22,28 @@ namespace HLA {
                              std::wstring&& ip) noexcept:
                                     BaseFederate(std::move(name),std::move(type),std::move(FOMname),std::move(fname),std::move(ip)){}
 									
-    SimFederate::SimFederate(const HLA::JSON& file) noexcept : BaseFederate(file){}
+    SimFederate::SimFederate(const HLA::JSON& file) noexcept :
+                                 BaseFederate(file){}
 		
-    SimFederate::SimFederate(HLA::JSON&& file) noexcept : BaseFederate(std::move(file)){}
+    SimFederate::SimFederate(HLA::JSON&& file) noexcept :
+                                 BaseFederate(std::move(file)){}
+                                 
+    void SimFederate::SendDataToRTI(){
+        rti1516e::AttributeHandleValueMap map;
+        for(const auto& data: output)
+            map[_AttributesMap[_MyClass][data.first]] = data.second;
+        try{
+            _rtiAmbassador->updateAttributeValues(_MyInstanceID,map, rti1516e::VariableLengthData());
+        }
+        catch(...){
+            HLA::Logger log(this->_log_filename);
+            log << L"ERROR"
+                << _federate_name
+                << L"Can't update attributes"
+                << HLA::Logger::Flush();
+                    
+        }
+    }
 	
     void SimFederate::ParameterProcess(){
         std::lock_guard<std::mutex> guard(_pmutex);
@@ -33,19 +53,37 @@ namespace HLA {
         }
     }
 
-    void SimFederate::AttributeProcess(){}
+    void SimFederate::AttributeProcess(){
+        std::lock_guard<std::mutex> guard(_amutex);
+        while(!_qAttributes.empty()){
+            auto& message = _qAttributes.front();
+            for(const auto& obj : _ObjectClasses){
+                for(const auto& attr_map : _AttributesMap[obj.second]){
+                    auto& data = message.data.find(attr_map.second);
+                    if(data != end(message.data))
+                        input[attr_map.first] = std::move(data->second);
+                }
+            }
+                
+            _qAttributes.pop();
+        }
+    }
+    
+    std::unordered_map<std::wstring, rti1516e::VariableLengthData>& SimFederate::getInputData() { 
+        this->AttributeProcess();
+        return input;
+    }
+    
+    std::unordered_map<std::wstring, rti1516e::VariableLengthData>& SimFederate::getOutputData() { 
+        return output;
+    }
 
     void SimFederate::SendParameters() const{
 
     }
 
     void SimFederate::UpdateAttributes() const{
-        rti1516e::VariableLengthData v = HLA::cast_to_rti<HLA::Wstring>(_federate_name);
-        int i = 20;
-        rti1516e::VariableLengthData type = HLA::cast_to_rti<HLA::Integer32BE>(i);
-        rti1516e::AttributeHandleValueMap map;
-        map[_AttributesMap.at(_MyClass).at(L"Name")] = v;
-        _rtiAmbassador->updateAttributeValues(_MyInstanceID,map,type);
+       
     }
 
 }
--- HLA-Simulink/SimFederate/SimFederate.hpp
+++ HLA-Simulink/SimFederate/SimFederate.hpp
@@ -3,39 +3,48 @@
 #include "BaseFederate.hpp"
 #include "HLAdata/HLAdata.hpp"
 namespace HLA {
-
-
+    
+    
     class SimFederate : public BaseFederate{
     public:
         SimFederate() noexcept;
-
+        
         SimFederate(const std::wstring& name,
-                      const std::wstring& type,
-                      const std::wstring& FOMname,
-                      const std::wstring& fname,
-                      const std::wstring& ip = L"localhost") noexcept;
-
+                const std::wstring& type,
+                const std::wstring& FOMname,
+                const std::wstring& fname,
+                const std::wstring& ip = L"localhost") noexcept;
+        
         SimFederate(std::wstring&& name,
-                      std::wstring&& type,
-                      std::wstring&& FOMname,
-                      std::wstring&& fname,
-                      std::wstring&& ip = L"localhost") noexcept;
-					  
+                std::wstring&& type,
+                std::wstring&& FOMname,
+                std::wstring&& fname,
+                std::wstring&& ip = L"localhost") noexcept;
+        
         SimFederate(const HLA::JSON& file) noexcept;
-		
+        
         SimFederate(HLA::JSON&& file) noexcept;
-
+        
+        void SendDataToRTI();
+        
+        std::unordered_map<std::wstring, rti1516e::VariableLengthData>& getInputData();
+        
+        std::unordered_map<std::wstring, rti1516e::VariableLengthData>& getOutputData();
+        
+        
     private:
-
+        
         void ParameterProcess() override;
-
+        
         void AttributeProcess() override;
-
+        
         void SendParameters() const override;
-		
-		void UpdateAttributes() const override;
-
-
-	};
+        
+        void UpdateAttributes() const override;
+        
+        std::unordered_map<std::wstring, rti1516e::VariableLengthData> input;
+        std::unordered_map<std::wstring, rti1516e::VariableLengthData> output;
+        
+    };
 }
 #endif // TRANSFER_HPP
