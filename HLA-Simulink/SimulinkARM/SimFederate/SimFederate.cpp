4823a283743ffcfb7032cd0bdc0bfee346b0dbfa
smilemaxdev <novoseltsev.as@phystech.edu>
2021-02-05 15:20:21 +0300

    HLAlib update model for ARM 5.02.2021

--- HLA-Simulink/SimulinkARM/SimFederate/SimFederate.cpp
+++ HLA-Simulink/SimulinkARM/SimFederate/SimFederate.cpp
@@ -6,55 +6,8 @@ namespace HLA {
     using namespace  rti1516e;
     
     extern std::unique_ptr<Logger> logger;
-
-	class HLAButton final : public HLA::BaseFixedRecord<Button,8>{
-	public:
-    void getDataMax(void *ptrSource, unsigned long uiMaxSize){
-        HLA::Integer32LE field1;
-        HLA::Integer32LE field2;
-        HLA::Integer32LE field3;
-		HLA::Wstring 	 field4;
-        unsigned offset = 0;
-        auto_offset(offset,ptrSource,uiMaxSize,
-                                                field1,
-                                                field2,
-                                                field3,
-												field4);
-    }
-    void get(Button const &obj){
-        HLA::Integer32LE field1;
-        HLA::Integer32LE field2;
-        HLA::Integer32LE field3;
-		HLA::Wstring 	 field4;
-        unsigned offset = 0, uiSize;
-        m_uiSizeData=0;
-        auto_geter_first(offset,uiSize,
-                                        field1,obj.push,
-                                        field2,obj.button_id,
-                                        field3,obj.bulb_id,
-										field4, obj.model_name);
-
-        auto_geter_second(offset,uiSize,
-                                        field1,
-                                        field2,
-                                        field3,
-										field4);
-    }
-    void set(Button &obj){
-        HLA::Integer32LE field1;
-        HLA::Integer32LE field2;
-        HLA::Integer32LE field3;
-		HLA::Wstring 	 field4;
-        unsigned offset = 0, uiSize;
-        auto_seter(offset,uiSize,
-                                    field1,obj.push,
-                                    field2,obj.button_id,
-                                    field3,obj.bulb_id,
-									field4, obj.model_name);
-    }
-};
-
-
+    
+    using HLAButton = Struct_Wrapper<HLA::Button, 8, Integer32LE, Integer32LE, Integer32LE, Wstring, Enum<Appoitment>>;
 
 
     SimFederate::SimFederate(const std::wstring& name,
@@ -91,30 +44,44 @@ namespace HLA {
 	
     void SimFederate::ParameterProcess(){
         std::lock_guard<std::mutex> guard(_pmutex);
-		rti1516e::InteractionClassHandle name = _InteractionClasses.at(L"ARMaction");
+		//rti1516e::InteractionClassHandle name = _InteractionClasses.at(L"ARMaction");
 		while(!_qParameters.empty()){
 			auto& message = _qParameters.front();
-			Button button = HLA::cast_from_rti<HLAButton>(message.data.find(_ParametersMap.at(name).at(L"PushButton"))->second);
-            *logger << L"INFO:" << _federate_name << L" Recive button with name " << button.model_name << Logger::Flush();
-			//if(button.model_name == _federate_name)
-				SendBulbFlashSignal(button.bulb_id);
+            if(message.handle == _InteractionClasses[L"ARMaction"){
+                Button button = HLA::cast_from_rti<HLAButton>(message.data[_ParametersMap[name][L"PushButton"])->second);
+                *logger << L"INFO:" << _federate_name << L" Recive button with name " << button.model_name << Logger::Flush();
+                if(button.model_name == _federate_name && button.action == Appoitment::ON_OFF){
+                    active_mode = !active_mode;
+                    SendBulbFlashSignal(button.bulb_id);
+                }
+            }
 			_qParameters.pop();
 		}
     }
 
-    void SimFederate::AttributeProcess(){}
+    void SimFederate::AttributeProcess(){
+        std::lock_guard<std::mutex> guard(_amutex);
+        while(!_qAttributes.empty()){
+            if(active_mode){
+                
+            }
+            _qAttributes.pop();
+        }
+    }
 
     void SimFederate::SendParameters() const{
 
     }
 
     void SimFederate::UpdateAttributes() const{
-       rti1516e::VariableLengthData v = HLA::cast_to_rti<HLA::Wstring>(_federate_name);
-        int i = 20;
-        rti1516e::VariableLengthData type = HLA::cast_to_rti<HLA::Integer32BE>(i);
-        rti1516e::AttributeHandleValueMap map;
-       map[_AttributesMap.at(_MyClass).at(L"Name")] = v;
-        _rtiAmbassador->updateAttributeValues(_MyInstanceID,map,type);
+        if(active_mode){
+            rti1516e::VariableLengthData v = HLA::cast_to_rti<HLA::Wstring>(_federate_name);
+            int i = 20;
+            rti1516e::VariableLengthData type = HLA::cast_to_rti<HLA::Integer32BE>(i);
+            rti1516e::AttributeHandleValueMap map;
+            map[_AttributesMap.at(_MyClass).at(L"Name")] = v;
+            _rtiAmbassador->updateAttributeValues(_MyInstanceID,map,type);
+        }
     }
 
 }
--- HLA-Simulink/SimulinkARM/SimFederate/SimFederate.hpp
+++ HLA-Simulink/SimulinkARM/SimFederate/SimFederate.hpp
@@ -3,18 +3,26 @@
 #include "BaseFederate.hpp"
 #include "HLAdata/HLAdata.hpp"
 namespace HLA {
-
+    
+    enum Appoitment{
+        ON_OFF,
+        DO_NOTHING
+    }
+    
 	struct Button{
 		int push = 0;
 		int button_id;
 		int bulb_id;
 		std::wstring model_name;
+        Appoitment action = Appoitment::DO_NOTHING;
 	};
 
 	struct Tumbler{
 		int state = 0;
 		std::string tumbler_id;
 	};
+    
+
 
 
 
@@ -38,6 +46,8 @@ namespace HLA {
         SimFederate(HLA::JSON&& file) noexcept;
 
     private:
+        
+        bool active_mode = false;
 	
 		void SendBulbFlashSignal(int&);
 
